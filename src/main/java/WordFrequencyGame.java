f83f030831b70bb72969ce91d1dfba3a5020f19c
AubBantolo <jhuliet14@gmail.com>
2023-08-21 20:00:17 +0800

    refactor: removed unnecessary checking for one word as stream caters it already

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
@@ -9,35 +11,29 @@ public class WordFrequencyGame {
     public static final String CALCULATE_ERROR = "Calculate Error";
 
     public String getResult(String inputStr) {
+        try {
 
-        if (inputStr.split(SPACE_DELIMITER).length == 1) {
-            return inputStr + " 1";
-        } else {
+            //split the input string with 1 to n pieces of spaces
+            String[] words = inputStr.split(SPACE_DELIMITER);
 
-            try {
+            List<WordFrequencyInfo> wordFrequencyInfoList = Arrays.stream(words)
+                    .map(s -> new WordFrequencyInfo(s, 1))
+                    .collect(Collectors.toList());
 
-                //split the input string with 1 to n pieces of spaces
-                String[] words = inputStr.split(SPACE_DELIMITER);
+            //get the map for the next step of sizing the same word
+            Map<String, List<WordFrequencyInfo>> wordListMap = getListMap(wordFrequencyInfoList);
 
-                List<WordFrequencyInfo> wordFrequencyInfoList = Arrays.stream(words)
-                        .map(s -> new WordFrequencyInfo(s,1))
-                        .collect(Collectors.toList());
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<WordFrequencyInfo>> wordListMap = getListMap(wordFrequencyInfoList);
-
-                return wordListMap.entrySet().stream()
-                        .map(stringListEntry -> new WordFrequencyInfo(stringListEntry.getKey(), stringListEntry.getValue().size()))
-                        .sorted((wordInfo1, wordInfo2) -> wordInfo2.getWordCount() - wordInfo1.getWordCount())
-                        .map(wordFrequencyInfo -> wordFrequencyInfo.getValue() + SPACE_CHAR + wordFrequencyInfo.getWordCount())
-                        .collect(Collectors.joining(NEW_DELIMITER));
+            return wordListMap.entrySet().stream()
+                    .map(stringListEntry -> new WordFrequencyInfo(stringListEntry.getKey(), stringListEntry.getValue().size()))
+                    .sorted((wordInfo1, wordInfo2) -> wordInfo2.getWordCount() - wordInfo1.getWordCount())
+                    .map(wordFrequencyInfo -> wordFrequencyInfo.getValue() + SPACE_CHAR + wordFrequencyInfo.getWordCount())
+                    .collect(Collectors.joining(NEW_DELIMITER));
 
 
-            } catch (Exception e) {
+        } catch (Exception e) {
 
 
-                return CALCULATE_ERROR;
-            }
+            return CALCULATE_ERROR;
         }
     }
 
